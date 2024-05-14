9d9103d77c697e40939eaa75766d0a19375f730f
TiTY <tee25450@gmail.com>
2024-05-14 13:55:35 +0700

    ex1-6(done)

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,5 @@
 // Start coding here
+let add = (a, b) => a + b;
+let subtarct = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,8 @@
 const minNumber = findMinNumber(10, 20);
 
 console.log(minNumber); // 10
+function findMinNumber(a, b) {
+  return Math.min(a, b);
+}
+const Result = findMinNumber(5, 9);
+console.log(Result);
