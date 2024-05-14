26b849e6f86c9a5b143efa26c7d2c937569a2260
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-05-14 14:23:08 +0700

    feat: done 1-6 exsercises

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,32 @@
 // Start coding here
-let calculator = {};
+const add = (a, b) => {
+  return a + b;
+};
+
+//
+const subtract = (a, b) => {
+  return a - b;
+};
+
+//
+const multiply = (a, b) => {
+  return a * b;
+};
+
+//
+const divide = (a, b) => {
+  return a / b;
+};
+
+let calculator = {
+  add: add,
+  subtract: subtract,
+  multiply: multiply,
+  divide: divide,
+};
+
+let addResult = calculator.add(10, 20);
+console.log(addResult);
+
+let divideResult = calculator.divide(3000, 10);
+console.log(divideResult);
